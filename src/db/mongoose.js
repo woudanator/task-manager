f95f0631117e9f0e8714b4fe8b37a4e51f59cab0
Reyn Wouda <reynw112@gmail.com>
2019-04-27 09:55:25 +0200

    Create Rest API Routes

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,12 +1,9 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 
-DBurl = 'mongodb://127.0.0.1:27017/task-manager-api';
+DBurl = 'mongodb://127.0.0.1:4321/task-manager-api';
 
 mongoose.connect(DBurl,{
     useNewUrlParser: true,
     useCreateIndex: true
 });
-
-
-
--- src/index.js
+++ src/index.js
@@ -8,6 +8,26 @@ const port = process.env.port || 3000
 
 app.use(express.json());
 
+app.get('/users',(req,res)=>{
+    User.find({}).then((users)=>{
+        res.send(users);
+    }).catch((err)=>{
+        res.status(500).send(err);
+    })
+});
+
+app.get('/users/:id',(req,res)=>{
+    const _id = req.params.id;
+    User.findById(_id).then((user)=>{
+        if(!user){
+          return  res.status(404).send();
+        }
+        res.send(user);
+    }).catch((err)=>{
+        res.status(500).send(err);
+    })
+})
+
 app.post('/users',(req,res)=>{
     const user = new User(req.body);
     user.save().then(()=>{
@@ -16,6 +36,26 @@ app.post('/users',(req,res)=>{
         res.status(400).send(e);
     })
 });
+
+app.get('/tasks',(req,res)=>{
+    Task.find({}).then((tasks)=>{
+        res.send(tasks);
+    }).catch(()=>{
+        res.status(500).send();
+    })
+})
+
+app.get('/tasks/:id',(req,res)=>{
+    const id = req.params.id;
+    Task.findById(id).then((task)=>{
+        if(!task){
+            return res.status(404).send();
+        }
+        res.send(task);
+    }).catch(err=>{
+        res.status(500).send();
+    })
+})
 
 app.post('/tasks',(req,res)=>{
     const task = new Task(req.body);
@@ -29,4 +69,4 @@ app.post('/tasks',(req,res)=>{
 
 
 app.listen(port,()=>{
-})
+});
