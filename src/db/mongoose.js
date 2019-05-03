fd979e5b896d723731c443aa6d5f0d1b98488edb
Reyn Wouda <reynw112@gmail.com>
2019-05-03 14:39:07 +0200

    Database path change

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 
-DBurl = 'mongodb://127.0.0.1:4321/task-manager-api';
+DBurl = 'mongodb+srv://reyn:R@Private1001@nodecourse-drhwz.mongodb.net/task-manager-api';
 
 mongoose.connect(DBurl,{
     useNewUrlParser: true,
