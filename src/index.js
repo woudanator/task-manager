15eae7c37efa148f2ecb3ce026d179406fbfc83b
Reyn Wouda <reynw112@gmail.com>
2019-05-10 16:27:28 +0200

    Tweaked the Routes for Express

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 const express = require('express');
 require('./db/mongoose');
-const User = require('./models/user');
-const Task = require('./models/task');
 const userRouter = require('./routers/user');
 const taskRouter = require('./routers/task');
-
 const app = express()
 const port = process.env.port || 3000
 
+app.use(express.json());
+
 app.use(userRouter);
 app.use(taskRouter);
-app.use(express.json());
+
 
 app.listen(port,()=>{
     console.log(`System is running on port: ${port}`);
--- src/routers/user.js
+++ src/routers/user.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = new express.Router();
+const User = require('../models/user');
 
 // Get all Users
 router.get('/users',async(req,res)=>{
